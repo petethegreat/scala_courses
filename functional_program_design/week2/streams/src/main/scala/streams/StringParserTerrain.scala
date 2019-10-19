346a181ac6e437e8b3c23a617271f62d9add95ee
Peter Thompson <petethegreat@gmail.com>
2019-10-19 14:32:53 -0400

    started project

--- functional_program_design/week2/streams/src/main/scala/streams/StringParserTerrain.scala
+++ functional_program_design/week2/streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,13 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = {
+    def isValid(p:Pos): Boolean = p match {
+      case Pos(r,c) => levelVector(r)(c) != '-'
+    }
+    isValid
+  }
+
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +68,7 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = new Pos()
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
