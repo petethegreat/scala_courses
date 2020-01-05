1f1a807daf463b4d3e53fb1af55b29e19531dd6e
Pete <petethegreat@gmail.com>
2020-01-05 15:30:59 -0500

    ugh

--- functional_program_design/week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ functional_program_design/week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -7,8 +7,24 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 // PT
+
+
+// def sizedheapGen(size:Int) : Gen[H]
+// https://booksites.artima.com/scalacheck/examples/html/ch06.html
+// https://stackoverflow.com/questions/3666744/sized-generators-in-scalacheck
+// https://alvinalexander.com/scala/scalacheck-custom-generator-examples
+// https://alvinalexander.com/scala/fp-book/quick-review-scala-for-expressions
+// https://www.scala-exercises.org/scalacheck/generators
+
+
   lazy val genHeap: Gen[H] = oneOf(
-    Const(IntHeap.empty),
+    Const(H.empty),
+    for {
+    	heapsize <- choose(5,10)
+    	i <- 1 to heapsize
+    	emp <- 
+
+    }
     
     
 
@@ -18,6 +34,8 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
     }
   )
+
+  //
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
