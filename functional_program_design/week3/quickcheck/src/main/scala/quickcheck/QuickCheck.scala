6935153207e22496aac1238b757395fb2cfb9509
pthompson-l1 <petethegreat@gmail.com>
2020-01-14 17:09:07 -0500

    almost done

--- functional_program_design/week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ functional_program_design/week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -5,6 +5,8 @@ import Arbitrary._
 import Gen._
 import Prop._
 
+import scala.annotation.tailrec
+
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 // PT
 
@@ -38,5 +40,47 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     val h = insert(b,insert(a,empty))
     findMin(h) == Math.min(a,b)
   }
+
+  property("insert_empty_delete_min") = forAll { a:Int =>
+    isEmpty(deleteMin(insert(a,empty)))
+  }
+
+//  def getMinSequence(theheap:H):List[A] =
+//    theheap match {
+//      case empty => Nil;
+//      case _ => findMin(theheap)::getMinSequence(deleteMin(theheap))}
+
+  def getMinSequence(theheap:H):List[A] =
+    if (isEmpty(theheap)) Nil else findMin(theheap)::getMinSequence(deleteMin(theheap))
+
+
+  property("findMin_gives_sorted_sequence") = forAll { h:H =>
+    lazy val thelist = getMinSequence(h)
+    thelist == thelist.sorted
+  }
+
+  property("melded_min_is_one_of_component_mins") = forAll { (h: H, g: H) =>
+    val mins = List(g, h).filter(x => !isEmpty(x)).map(x => findMin(x))
+    if (mins.length == 0) true else mins.contains(findMin(meld(g, h)))
+  }
+
+  property("min_after_deletion_is_greater") = forAll{ (h:H) =>
+    h match {
+      case x::x1::xs => findMin(h) <= findMin(deleteMin(h))
+      case _ => true}
+  }
+
+  property("insertions_preserved_for_non_min") = forAll{ (a:Int,b:Int) =>
+    findMin(deleteMin(insert(b,insert(a,empty)))) == Math.max(a,b)
+  }
+
+  }
+
+
+// test properties
+
+
+
+
 
 }
