fee72d1013c1a8cf975b68d77a215eb27f188b87
Pete <petethegreat@gmail.com>
2020-08-28 22:43:54 -0400

    vis

--- capstone/observatory/src/main/scala/observatory/Visualization.scala
+++ capstone/observatory/src/main/scala/observatory/Visualization.scala
@@ -1,7 +1,9 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
-  import scala.math.{Pi, toRadians, sin, cos, acos, abs}
+
+import scala.annotation.tailrec
+import scala.math.{Pi, abs, acos, cos, sin, toRadians, pow}
 
 /**
   * 2nd milestone: basic visualization
@@ -14,6 +16,9 @@ object Visualization extends VisualizationInterface {
     * @return The predicted temperature at `location`
     */
     val TOLERANCE = 1.0e-9
+  val closeEnough = 1/6400.0 // should be 1km
+  val inverseDistanceP = 2.0
+
 
     def getLocationDifference(l1: Location, l2:Location): Double = {
 //      return a delta sigma difference in radians
@@ -31,6 +36,23 @@ object Visualization extends VisualizationInterface {
 //      compute difference in angles between locations
       temperatures.map( x => (getLocationDifference(x._1,location),x._2))
     }
+
+  def aggDeltaSigmaTemp(dsigmaTemp: Iterable[(Double, Temperature)]): Temperature = {
+    val inverseDistances = dsigmaTemp.map( k => (pow(k._1,-inverseDistanceP), k._2))
+    val inverseDistanceSums = inverseDistances.foldLeft((0.0,0.0))( (uw_u, id_t) => (uw_u._1 + id_t._1 * id_t._2, uw_u._2 + id_t._1))
+    inverseDistanceSums._1/inverseDistanceSums._2
+  }
+
+  def computeMeanTemperature(dsigmaTemp: Iterable[(Double, Temperature)]): Temperature = {
+    // check if there is a zeroish distance observation
+
+    val here = dsigmaTemp.collectFirst({case a if abs(a._1) < closeEnough => a._2})
+    here match {
+      case Some(temp) => temp
+      case None => aggDeltaSigmaTemp(dsigmaTemp)
+    }
+  }
+
 
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
     ???
--- capstone/observatory/src/test/scala/observatory/VisualizationTest.scala
+++ capstone/observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -2,6 +2,7 @@ package observatory
 
 import org.junit.Assert._
 import org.junit.Test
+import math.abs
 
 trait VisualizationTest extends MilestoneSuite {
   private val milestoneTest = namedMilestoneTest("raw data display", 2) _
@@ -16,8 +17,8 @@ trait VisualizationTest extends MilestoneSuite {
   assert(expected == actual, s"expected ${expected}, actual ${actual}")
 }
   @Test def `visualisation: check deltasigma antipodes` = {
-    val l1 = Location(87.0,36.5)
-    val l2 = Location(-87.0,216.5)
+    val l1 = Location(87.0,-90.5)
+    val l2 = Location(-87.0,89.5)
     val expected = math.Pi
     val actual = Visualization.getLocationDifference(l1,l2)
     assert(expected == actual, s"expected ${expected}, actual ${actual}")
@@ -28,7 +29,37 @@ trait VisualizationTest extends MilestoneSuite {
     val l2 = Location(80.0,216.5)
     val expected = 0.17453292519943317
     val actual = Visualization.getLocationDifference(l1,l2)
-    assert(math.abs(expected - actual) < TOLERANCE, s"expected ${expected}, actual ${actual}")
+    assert(abs(expected - actual) < TOLERANCE, s"expected ${expected}, actual ${actual}")
   }
+
+  @Test def `visualisation: aggDeltaSigmaTemp sums correctly` = {
+    val dsigma_temp:Iterable[(Double,Temperature)] = Iterable( (1.0,40.0), (2.0,50.0))
+    val actual = Visualization.aggDeltaSigmaTemp(dsigma_temp)
+    val expected = 42.0
+    assert(abs(actual - expected) < TOLERANCE, s"expected ${expected}, actual ${actual}")
+  }
+
+  @Test def `visualisation: computeMeanTemperature returns zero distance result` = {
+    val observations = Iterable(
+      (3.0, 3.0),
+      (0.5, 9.7),
+      (1.1, 19.7),
+      (2.0e-4, 99.7),
+      (1.0e-4, 36.5)
+    )
+
+    val expected = 36.5
+    val actual = Visualization.computeMeanTemperature(observations)
+    assert(abs(actual - expected) < TOLERANCE, s"expected ${expected}, actual ${actual}")
+  }
+
+
+
+
+
+
+//  computeMeanTemperature - case when distance 0
+//  aggDeltaSigmaTemp = quick check
+
 
 }
