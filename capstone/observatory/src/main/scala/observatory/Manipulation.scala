b0f5e89ab977372068bd426505157cd90c149967
Pete <petethegreat@gmail.com>
2020-11-01 16:29:13 -0500

    manipulation 1

--- capstone/observatory/src/main/scala/observatory/Manipulation.scala
+++ capstone/observatory/src/main/scala/observatory/Manipulation.scala
@@ -1,4 +1,6 @@
 package observatory
+import Visualization.predictTemperature
+import scala.collection
 
 /**
   * 4th milestone: value-added information
@@ -10,8 +12,36 @@ object Manipulation extends ManipulationInterface {
     * @return A function that, given a latitude in [-89, 90] and a longitude in [-180, 179],
     *         returns the predicted temperature at this location
     */
+    val (gridLonMin, gridLonMax) = (-180, 179)
+    val (gridLatMin, gridLatMax) = (-89, 90)
+    val debug = true
+    val DEFAULT_TEMPERATURE: Temperature = -999
+
+
+
+// dunno, this might be handy
+  def locationToGridLocation(loc:Location): GridLocation = {
+    val glat:Int = loc.lat.round.toInt
+    val glon:Int = loc.lon.round.toInt
+    GridLocation(glat, glon)
+  }
+
+
   def makeGrid(temperatures: Iterable[(Location, Temperature)]): GridLocation => Temperature = {
-    ???
+    if (debug )  println("generating grid")
+
+
+    val gridLocs = for (
+      glat <- gridLatMin to gridLatMax;
+      glon <- gridLonMin to gridLonMax
+    ) yield GridLocation(glat, glon)
+
+    val tempCache = gridLocs.map(
+      x => x -> predictTemperature(temperatures, Location(x.lat.toFloat,x.lon.toFloat))).toMap
+
+    def lookup(gl:GridLocation): Temperature = tempCache.getOrElse(gl,DEFAULT_TEMPERATURE)
+    return lookup
+
   }
 
   /**
@@ -20,8 +50,26 @@ object Manipulation extends ManipulationInterface {
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
   def average(temperaturess: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature = {
-    ???
+
+    val Ntempss = temperaturess.size.toFloat
+
+    val gridLocs = for (
+      glat <- gridLatMin to gridLatMax;
+      glon <- gridLonMin to gridLonMax
+    ) yield GridLocation(glat, glon)
+
+    val averages = collection.mutable.Map[GridLocation,Temperature]() ++ gridLocs.map(x => x -> 0.0).toMap
+
+    for (
+      temps <- temperaturess;
+      looker = makeGrid(temps);
+      gl <- gridLocs
+    ) { averages(gl) += looker(gl)/Ntempss}
+
+    def averagelookup(gl: GridLocation) = averages.getOrElse(gl,DEFAULT_TEMPERATURE)
+    return averagelookup
   }
+
 
   /**
     * @param temperatures Known temperatures
@@ -29,9 +77,13 @@ object Manipulation extends ManipulationInterface {
     * @return A grid containing the deviations compared to the normal temperatures
     */
   def deviation(temperatures: Iterable[(Location, Temperature)], normals: GridLocation => Temperature): GridLocation => Temperature = {
-    ???
+
+    val currentTemps = makeGrid(temperatures)
+    
+    def lookup(gl: GridLocation) = currentTemps(gl) - normals(gl)
+    return lookup
+
   }
 
 
 }
-
