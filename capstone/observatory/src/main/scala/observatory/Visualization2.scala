de9c0f2c6b8923729db50ca516cde940f5f7b226
Pete <petethegreat@gmail.com>
2020-11-02 20:41:27 -0500

    8/10, barely adequate

--- capstone/observatory/src/main/scala/observatory/Visualization2.scala
+++ capstone/observatory/src/main/scala/observatory/Visualization2.scala
@@ -16,6 +16,21 @@ object Visualization2 extends Visualization2Interface {
     * @return A guess of the value at (x, y) based on the four known values, using bilinear interpolation
     *         See https://en.wikipedia.org/wiki/Bilinear_interpolation#Unit_Square
     */
+
+  val DEFAULT_COLORMAP = Seq(
+    (7.0,  Color(0,0,0)),
+    (4.0,  Color(255,0,0)),
+    (2.0,  Color(255,255,0)),
+    (0.0,  Color(255,255,255)),
+    (-2.0, Color(0,255,255)),
+    (-7.0, Color(0,0,255)),
+  )
+
+  val NPIX = 256
+  val SCALE = 1
+
+
+
   def bilinearInterpolation(
     point: CellPoint,
     d00: Temperature,
@@ -23,7 +38,10 @@ object Visualization2 extends Visualization2Interface {
     d10: Temperature,
     d11: Temperature
   ): Temperature = {
-    ???
+    d00*(1.0 - point.x)*(1.0 - point.y) +
+      d01*(1.0 - point.x )*point.y +
+      d10*point.x*(1.0 - point.y) +
+      d11*point.x*point.y
   }
 
   /**
@@ -39,5 +57,37 @@ object Visualization2 extends Visualization2Interface {
   ): Image = {
     ???
   }
+
+ //  def gridTile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
+ //
+ // // get the locations in the tile (Scale)
+ // // map the location to 4 gridpoints, and the CellPoint
+ // // interpolate to get the estimated temperature
+ // // map temperature to color
+ //
+ //
+ //    val (st_indices, st_locs) = GetSubTileLocations(tile,SCALE)
+ //
+ //    // this should be done in parallel
+ //    val indexed_colours = st_indices.zip(st_locs).par.map {
+ //      case (indices, loc) => (indices, interpolateColor(colors,predictTemperature(temperatures, loc)) )}
+ //
+ //    // output
+ //    val alpha = 127
+ //    val out = new Array[Pixel](Npix*Npix)
+ //    // add a little loop here, or define a function to write adjacent pixels if scalefac > 1
+ //    for (
+ //      (indices, cc) <- indexed_colours;
+ //      ii_scale <- 0 until SCALE;
+ //      jj_scale <- 0 until SCALE;
+ //      ix = indices._1 + ii_scale;
+ //      jy = indices._2 + jj_scale;
+ //      if (ix < Npix) ;
+ //      if (jy < Npix)
+ //    ) {out(ix + Npix*jy) = Pixel(cc.red, cc.green, cc.blue, alpha) }
+ //    // indexed_colours.foreach {case (ij,cc) => out(ij._1 + Npix*ij._2) = Pixel(cc.red, cc.green, cc.blue, alpha)}
+ //    // scalefac
+ //    Image(Npix,Npix,out)
+ //  }
 
 }
